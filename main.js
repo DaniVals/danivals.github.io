2edf6f6984bcd859182fb260fd77aa41bfa1b8e3
Daniel <daniel.vals.simon@gmail.com>
2024-02-26 19:17:46 +0100

    PC

---
+++ main.js
@@ -0,0 +1,69 @@
+async function loadMain(){
+    //paso 1 hacer cosas que no quieras con transicion
+    if(getCookie('darkMode')=='true'){document.getElementById('darkMode').checked = true}
+    else{document.getElementById('darkMode').checked = false}
+    refreshDarkMode()
+    flipSvg('infoSvg',-90);
+
+    await sleep(1) //esperar 1ms
+    
+    //añadir transiciones a dichas cosas
+    document.documentElement.style.setProperty('--CeroCincoSecs', '0.5s');
+    console.log("index cargado")
+}
+function añadirEase(cambiado,tiempo){
+    //atributo css-> transition: 0.5s ease;
+    cambiado.style.transition = (tiempo+"s ease")
+}
+
+
+
+
+
+function expandirDiv(selected,svg) {
+
+    let div = document.getElementById(selected);
+    let divHeight = div.style.height;
+
+    if(divHeight==0||divHeight=="0px"){
+
+        div.style.height=div.scrollHeight +'px'
+        if(svg != null){
+            flipSvg(svg,0);
+        }
+    }else{
+        
+        div.style.height='0px'
+        if(svg != null){
+            flipSvg(svg,-90);
+        }
+    }
+}
+function flipSvg(svg, giro) {
+    let flipp = document.getElementById(svg);
+    flipp.setAttribute("transform", "rotate(" + giro + ")");
+}
+
+
+
+
+
+function copyText(copiar, indicar) {
+    navigator.clipboard.writeText(copiar)
+    .then(() => {
+        console.log('Texto copiado al portapapeles: ' + copiar);
+        cambiarTextoTemporal(indicar, 'Copiado!', 1000)
+    })
+    .catch(err => {
+        console.error('Error al copiar al portapapeles: ', err);
+        cambiarTextoTemporal(indicar, 'Error copiando el texto', 1000)
+    });
+}
+async function cambiarTextoTemporal(indicar, textoIndicado, tiempoms){
+
+    let textoOG = document.getElementById(indicar).innerHTML;
+    console.log(textoOG)
+    document.getElementById(indicar).innerHTML = textoIndicado;
+    await sleep(tiempoms)
+    document.getElementById(indicar).innerHTML = textoOG;
+}
